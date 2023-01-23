774b75c10f98cdd36b3bc5c854e1744751bc182e
Ayden Jahola <mm5621333@gmail.com>
2023-01-23 11:25:14 +0000

    poetry solution

--- ca117/week_01b/poetry_012.py
+++ ca117/week_01b/poetry_012.py
@@ -4,9 +4,9 @@ import sys
 
 lines = sys.stdin.readlines()
 max_len = max(len(line) for line in lines)
+max_len = max_len - 1
 
 for line in lines:
     line = line.rstrip()
-    spaces = " " * int((max_len - len(line)) / 2) + " "
-    print(f"{spaces}{line}")
+    print(f"{line:^{max_len}}")
 
