1ae49a4ce4fa0f1ddb64e72f28e2be5f66dc9b51
Ayden Jahola <mm5621333@gmail.com>
2023-03-13 11:31:17 +0000

    ca170

--- ca170/shell_scripting/gnews.sh
+++ ca170/shell_scripting/gnews.sh
@@ -10,7 +10,7 @@ fi
 wget -qO- "https://www.rte.ie/news/" |
 
 # Search the page for the argument using grep
-grep "$1" |
+grep -i "$1" |
 
 # Print the result
 # The sed command removes any HTML tags from the output
