8fa367b2eb5c450f085f3008dde008ff2daf2f6c
Ayden Jahola <jaholaayden@gmail.com>
2023-04-11 13:43:50 +0100

    week_11b

--- ca117/week_11b/mp3track_v2_122.py
+++ ca117/week_11b/mp3track_v2_122.py
@@ -13,17 +13,3 @@ class MP3Track(object):
         r.append('By: {}'.format(', '.join(self.artist)))
         r.append('Duration: {}'.format(self.duration))
         return '\n'.join(r)
-
-from mp3track_v2_122 import MP3Track
-
-def main():
-    t1 = MP3Track('Fools Gold', 604, ['The Stone Roses'])
-    t2 = MP3Track('Shallow', 197, ['Lady Gaga', 'Bradley Cooper'])
-    t3 = MP3Track('Telephone', 220, ['Beyonce', 'Lady Gaga'])
-
-    print(t1)
-    print(t2)
-    print(t3)
-
-if __name__ == '__main__':
-    main()
--- ca117/week_11b/mp3track_v3_122.py
+++ ca117/week_11b/mp3track_v3_122.py
@@ -13,21 +13,3 @@ class MP3Track(object):
         r.append('By: {}'.format(', '.join(self.artist)))
         r.append('Duration {}'.format(
         return '\n'.join(r)
-
-from mp3track_v3_122 import MP3Track
-
-def main():
-    t1 = MP3Track('Fools Gold', 604, ['The Stone Roses'])
-    t2 = MP3Track('Shallow', 197, ['Lady Gaga', 'Bradley Cooper'])
-    t3 = MP3Track('Telephone', 220, ['Beyonce', 'Lady Gaga'])
-    t4 = MP3Track('Her Majesty', 34, ['The Beatles'])
-    t5 = MP3Track('Seven Seconds', 7, ['Neneh Cherry'])
-
-    print(t1)
-    print(t2)
-    print(t3)
-    print(t4)
-    print(t5)
-
-if __name__ == '__main__':
-    main()
