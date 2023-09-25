33174217c67adba59ac63ba7a0dfae9779030f1d
Ayden Jahola <jaholaayden@gmail.com>
2023-09-25 23:10:57 +0100

    ca284: add the desired output for question 1

--- year-2/ca284/week_02/lab2-cylinder-area.c
+++ year-2/ca284/week_02/lab2-cylinder-area.c
@@ -4,9 +4,21 @@
 #define PI 3.1415
 
 int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    if (argc == 1)
+      printf("No input given!\n");
+    else
+      printf("Two arguments needed!\n");
+    return 1;
+  }
 
   double radius = atof(argv[1]);
   double height = atof(argv[2]);
+
+  if (radius < 0 || height < 0) {
+    printf("The radius or height cannot be negative!\n");
+    return 1;
+  }
 
   double area = 2 * PI * radius * (radius + height);
 
