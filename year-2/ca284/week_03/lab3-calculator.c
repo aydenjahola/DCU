bc36eea35d03f0fb4e4e31c2151c859c74e6283e
Ayden Jahola <jaholaayden@gmail.com>
2023-09-30 19:34:15 +0100

    ca284: update the calculator task to include factorial

--- year-2/ca284/week_03/lab3-calculator.c
+++ year-2/ca284/week_03/lab3-calculator.c
@@ -23,27 +23,49 @@ float divide(float num1, float num2) {
   return num1 / num2;
 }
 
+// Function to calculate the factorial of a number
+int factorial(int num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
 int main(int argc, char *argv[]) {
 
   // Get the operator and convert it to lowercase for case-insensitive
   // comparison
-  char operator[10];
+  char operator[20];
   strcpy(operator, argv[1]);
   for (int i = 0; operator[i]; i++) {
     operator[i] = tolower(operator[i]);
   }
 
-  // Convert the input numbers from command-line arguments to floating-point
-  // numbers
+  // Convert the input numbers from command-line arguments to floating-point or
+  // integer numbers
   float num1 = atof(argv[2]);
   float num2 = atof(argv[3]);
 
   // Perform the calculation based on the operator
   float result;
-  if (strcmp(operator, "multiply") == 0) {
+  if (strcmp(operator, "add") == 0) {
+    result = num1 + num2;
+  } else if (strcmp(operator, "subtract") == 0) {
+    result = num1 - num2;
+  } else if (strcmp(operator, "multiply") == 0) {
     result = multiply(num1, num2);
   } else if (strcmp(operator, "divide") == 0) {
     result = divide(num1, num2);
+  } else if (strcmp(operator, "factorial") == 0) {
+    if (num1 < 0) {
+      printf("Factorial is undefined for negative numbers\n");
+      exit(1);
+    }
+    result = factorial((int)num1);
+  } else {
+    printf("Invalid operator: %s\n", operator);
+    exit(1);
   }
 
   // Print the result
