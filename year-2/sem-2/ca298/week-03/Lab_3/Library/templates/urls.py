4fb48f3de898f2c37172abf073421fc4501600a8
Ayden Jahola <jaholaayden@gmail.com>
2024-01-29 15:41:54 +0000

    fix some styling and add navbar

--- year-2/sem-2/ca298/week-03/Lab_3/Library/templates/urls.py
+++ year-2/sem-2/ca298/week-03/Lab_3/Library/templates/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('books', views.view_all_books, name="all_books"),
-    path('books/<int:bookid>', views.view_single_book, name='single_book'),
-    path('books/year/<int:year>', views.view_books_by_year, name='books_by_year'),
-    path('books/category/<str:category>',
+    path('books/', views.view_all_books, name="all_books"),
+    path('books/<int:bookid>/', views.view_single_book, name='single_book'),
+    path('books/year/<int:year>/', views.view_books_by_year, name='books_by_year'),
+    path('books/category/<str:category>/',
          views.view_books_by_catergory, name='books_by_category'),
-    path('books/category/<str:category>/year/<int:year>',
+    path('books/category/<str:category>/year/<int:year>/`',
          views.view_books_by_year_and_category, name='books_by_year_and_category')
 ]
